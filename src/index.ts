e30848f834ccc26af64d210923234dd4f6488cb4
Willy van Vroenhoven <vroenhoven@opinio.nl>
2019-01-05 20:20:29 +0100

    Updated barrel file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-﻿import * as Framework from "./framework/index";
-import * as Controls from "./controls/index";
+﻿import * as Controls from "./controls/index";
+import * as Framework from "./framework/index";
+import * as Style from "./style/style";
+import * as Types from "./types/types";
 
-export { Framework };
 export { Controls };
+export { Framework };
+export { Style };
+export { Types }
