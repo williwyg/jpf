b8df153aee9ae9fc542f7827a3c3ac69723051e8
williwyg <34509739+williwyg@users.noreply.github.com>
2019-01-07 00:23:12 +0100

    Delete index.d.ts

--- declarations/framework/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./attribute/attribute";
-export * from "./dialog/dialog";
-export * from "./frameworkElement/frameworkElement";
-export * from "./inputElement/inputElement";
-export * from "./itemsElement/itemsElement";
