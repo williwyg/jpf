ee3ac58beb0aa9560996f880ec2814bf110a0a93
williwyg <34509739+williwyg@users.noreply.github.com>
2019-01-07 00:21:19 +0100

    Delete passwordBox.d.ts

--- declarations/controls/input/passwordBox.d.ts
+++
@@ -1,6 +0,0 @@
-import { TextBox, TextBoxOptions } from "./textBox";
-export declare class PasswordBox extends TextBox {
-    constructor(options?: PasswordBoxOptions);
-}
-export interface PasswordBoxOptions extends TextBoxOptions {
-}
