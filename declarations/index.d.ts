19dc1a623ba95ff3a910de8784de582bf8491fc3
williwyg <34509739+williwyg@users.noreply.github.com>
2019-01-07 00:23:39 +0100

    Delete index.d.ts

--- declarations/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Controls from "./controls/index";
-import * as Framework from "./framework/index";
-import * as Style from "./style/style";
-import * as Types from "./types/types";
-export { Controls };
-export { Framework };
-export { Style };
-export { Types };
