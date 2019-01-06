9085f3afa17e2766101bf90357f87ae7fcf0a127
Willy van Vroenhoven <vroenhoven@opinio.nl>
2019-01-06 15:13:51 +0100

    Added control PasswordBox

---
+++ declarations/controls/textBox/passwordBox.d.ts
@@ -0,0 +1,6 @@
+import { TextBox, TextBoxOptions } from "./textBox";
+export declare class PasswordBox extends TextBox {
+    constructor(options?: PasswordBoxOptions);
+}
+export interface PasswordBoxOptions extends TextBoxOptions {
+}
---
+++ src/controls/textBox/passwordBox.ts
@@ -0,0 +1,21 @@
+﻿import { TextBox, TextBoxOptions} from "./textBox";
+
+export class PasswordBox extends TextBox {
+    constructor(options?: PasswordBoxOptions) {
+        super(options);
+
+        this.type = "PasswordBox";
+
+        const buildSuper = this.build;
+        this.build = () => {
+            buildSuper();
+
+            var input = this.element as HTMLInputElement;
+            input.type = "password";
+        }
+    }  
+}
+
+export interface PasswordBoxOptions extends TextBoxOptions {
+    
+}
