4ba8063c16f90f1f89d8ce80cc2e0d4a95ee096b
williwyg <34509739+williwyg@users.noreply.github.com>
2019-01-07 00:20:54 +0100

    Delete div.d.ts

--- declarations/controls/div/div.d.ts
+++
@@ -1,6 +0,0 @@
-import { FrameworkElement, FrameworkElementOptions } from "../../framework/frameworkElement/frameworkElement";
-export declare class Div extends FrameworkElement {
-    constructor(options?: DivOptions);
-}
-export interface DivOptions extends FrameworkElementOptions {
-}
