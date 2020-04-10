5f2cb321bf72f22e9eecae10ed207db188cd7480
williwyg <34509739+williwyg@users.noreply.github.com>
2020-04-10 13:30:27 +0200

    Delete index.d.ts

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as Types from "./types";
-export * from "./eventListener";
-export * from "./uiElement";
-export * from "./style";
-export { userAgent } from "./userAgent";
-export { Types };
